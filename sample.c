99649e0b2f9a568361e8bf09429ead436e888440
Andrew Lopes de Oliveira <andrewskt@gmail.com>
2015-07-19 17:16:15 -0300

    Adcionado sample2.c e uma alinha a sample.c

--- sample.c
+++ sample.c
@@ -8,5 +8,6 @@ int main(){
     return 0; 
     
     printf("Versao 2"); 
+    printf("Versao 3"); 
 
 }
---
+++ sample2.c
@@ -0,0 +1,12 @@
+#include <stdlib.h> 
+
+
+int main ()
+
+{
+
+int a = 1+1; 
+
+printf("%i", a); 
+
+}
